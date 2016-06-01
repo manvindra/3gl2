d4aa703e56c5eadaef5ff8746b0d79b3fed501fd
manvindra singh <manvindra.singh@gmail.com>
2016-06-01 21:55:43 +0530

    initail

---
+++ js/bathroom_tub/walls2.js
@@ -0,0 +1,40 @@
+{
+
+"metadata":
+{
+"sourceFile": "test_manu.max",
+"generatedBy": "3ds max ThreeJSExporter",
+"formatVersion": 3.1,
+"vertices": 4,
+"normals": 2,
+"colors": 0,
+"uvs": 4,
+"triangles": 2,
+"materials": 1
+},
+
+"materials": [
+{
+"DbgIndex" : 0,
+"DbgName"  : "01 - Default",
+"colorDiffuse"  : [0.5882, 0.5882, 0.5882],
+"colorAmbient"  : [0.5882, 0.5882, 0.5882],
+"colorSpecular"  : [0.9000, 0.9000, 0.9000],
+"transparency"  : 1.0,
+"specularCoef"  : 10.0,
+"vertexColors" : false
+}
+
+],
+
+"vertices": [-11641.8,0.00256916,-13020.9,-11641.8,0.00123044,17605.6,-11641.8,32000.0,17605.6,-11641.8,32000.0,-13020.9],
+
+"normals": [1.0,0.0,0.0,1.0,0.0,0.0],
+
+"colors": [],
+
+"uvs": [[0.00388777,0.997876,0.00388777,0.00740987,0.994354,0.00740987,0.994354,0.997876]],
+
+"faces": [42,2,1,0,0,0,1,2,0,0,0,42,0,3,2,0,2,3,0,1,1,1]
+
+}
